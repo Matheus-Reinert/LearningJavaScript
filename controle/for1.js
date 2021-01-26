e5eba6844104223cdb22bb65c57c9f8f18daebdb
Matheus-Reinert <reinert.matheus@hotmail.com>
2021-01-26 10:58:40 -0300

    estruturas de controle

---
+++ controle/for1.js
@@ -0,0 +1,17 @@
+// diferença entre for e while
+
+let contador = 1;
+while(contador <= 10){
+    console.log(`contador = ${contador}`);
+    contador ++;
+}
+
+for(let i = 1; i <= 10; i ++){
+    console.log(`i = ${i}`);
+}
+
+// for com arrays
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+for(let i = 0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`);
+}
